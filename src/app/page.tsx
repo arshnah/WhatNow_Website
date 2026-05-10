aae6c1b96d18100184094adcf32e8d0168992fa2
Lakshya <antinityfx@gmail.com>
2026-05-10 12:38:42 +0530

    fix: minor bugfixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,7 @@
 import Hero from "@/components/home/Hero";
 
 export default function Home() {
-  return (
-    <div className="min-h-screen flex flex-col bg-white">
-      <Hero />
-    </div>
-  );
+  return (<>
+    <Hero />
+  </>);
 }
