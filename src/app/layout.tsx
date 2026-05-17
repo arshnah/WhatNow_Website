9cabf7d04abba27ebef6b9b52a9a1d96eb57e468
Lakshya <antinityfx@gmail.com>
2026-05-17 23:33:20 +0530

    feat: change font to geist sans

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,11 @@
 import type { Metadata } from "next";
 import Navbar from "@/components/Navbar";
-import { Nunito, Nunito_Sans } from "next/font/google";
+import { Geist } from "next/font/google";
 import "./globals.css";
 
-const nunito = Nunito({
-  variable: "--font-nunito",
+const geistSans = Geist({
+  variable: "--font-geist-sans",
   subsets: ["latin"],
-  weight: ["700", "800"],
-});
-
-const nunitoSans = Nunito_Sans({
-  variable: "--font-nunito-sans",
-  subsets: ["latin"],
-  weight: ["400", "500", "600"],
 });
 
 export const metadata: Metadata = {
@@ -26,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${nunito.variable} ${nunitoSans.variable} h-full antialiased`}>
+    <html lang="en" className={`${geistSans.variable} h-full antialiased`}>
       <body className="min-h-full flex flex-col font-sans bg-white text-slate-900">
         <Navbar />
 
